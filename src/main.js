e7e37042c22d445f5c49fa8c652ca42ef417e9cf
jdromero88 <jdromerov88@gmail.com>
2025-12-01 22:49:00 -0500

    Add magnifier component

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import './AudioHighlight.svelte';
 import './SplitFlap.svelte';
 import './AuthorGrid.svelte';
 import './lib/split-landing-page/Container.svelte';
-import './lib/split-landing-page/Left.svelte'
-import './lib/split-landing-page/Right.svelte'
+// import './lib/split-landing-page/Left.svelte'
+// import './lib/split-landing-page/Right.svelte'
 import './lib/test/Testing.svelte'
+import './lib/magnifier/Magnifier.svelte'
