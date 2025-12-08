05a1249923dfaf7f76cadb3787845166c319f336
Mariel de la Garza <mariel.code@gmail.com>
2025-12-08 13:09:16 -0500

    Set up dev-only build for use in testing

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,31 @@
 // vite.config.js
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
-export default defineConfig({
-  plugins: [svelte()],
-  build: {
-    lib: {
-      entry: 'src/main.js',
-      name: 'Widgets',
-      formats: ['iife'],
-      fileName: () => `widgets.js`
+export default defineConfig(({ mode }) => {
+  // When we run: vite build --mode dev-bundle
+  const isDevBundle = mode === "dev-bundle";
+
+  return {
+    plugins: [svelte()],
+    build: {
+      lib: {
+        // normal build uses src/main.js; dev bundle uses src/dev-main.js
+        entry: isDevBundle ? "src/dev-main.js" : "src/main.js",
+
+        //  global name for the IIFE bundle (same for both)
+        name: "Widgets",
+
+        // immediately invoked function expression (iife) - everything
+        // wrapped in a function that runs immediately
+        formats: ["iife"],
+
+        // different filenames for prod v. dev bundle
+        fileName: () => (isDevBundle ? "widgets-dev.js" : `widgets.js`),
+      },
+      rollupOptions: {
+        // keep everything bundled
+      },
     },
-    rollupOptions: {
-      // keep everything bundled
-    }
-  }
+  };
 });
