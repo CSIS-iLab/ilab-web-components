408214f4431c701665ae393ef57b484c53ef528f
jdromero88 <jdromerov88@gmail.com>
2026-01-10 16:25:27 -0500

    Create attributes to control btn bg color

---
+++ src/dev/split-landing-page/Left.svelte
@@ -0,0 +1,14 @@
+<script>
+  let { container, leftTitle, btnText } = $props();
+  let left;
+</script>
+
+<div
+  class="split left"
+  bind:this={left}
+  on:mouseenter={() => container?.classList.add("hover-left")}
+  on:mouseleave={() => container?.classList.remove("hover-left")}
+>
+  <h1>{leftTitle}</h1>
+  <a href="#" class="button">{btnText}</a>
+</div>
