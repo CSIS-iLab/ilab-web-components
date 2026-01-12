de96b6fc51fdffd2580196b218b00ef6c53396b9
Mariel de la Garza <mariel.code@gmail.com>
2026-01-12 15:13:47 -0500

    Update Svelte syntax to clear warnings

--- src/dev/split-landing-page/Left.svelte
+++ src/dev/split-landing-page/Left.svelte
@@ -6,8 +6,9 @@
 <div
   class="split left"
   bind:this={left}
-  on:mouseenter={() => container?.classList.add("hover-left")}
-  on:mouseleave={() => container?.classList.remove("hover-left")}
+  onmouseenter={() => container?.classList.add("hover-left")}
+  onmouseleave={() => container?.classList.remove("hover-left")}
+  role="presentation"
 >
   <h1>{leftTitle}</h1>
   <a href={btnLeftLink} target={leftTarget} class="button">{btnText}</a>
