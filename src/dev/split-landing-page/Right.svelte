f4211653a253355af6759977167ec34df0c00a23
Mariel de la Garza <mariel.code@gmail.com>
2026-01-12 15:14:26 -0500

    Upddate Svelte snytax and add aria role to clear warnings

--- src/dev/split-landing-page/Right.svelte
+++ src/dev/split-landing-page/Right.svelte
@@ -6,8 +6,9 @@
 <div
   class="split right"
   bind:this={right}
-  on:mouseenter={() => container?.classList.add("hover-right")}
-  on:mouseleave={() => container?.classList.remove("hover-right")}
+  onmouseenter={() => container?.classList.add("hover-right")}
+  onmouseleave={() => container?.classList.remove("hover-right")}
+  role="presentation"
 >
   <h1>{rightTitle}</h1>
   <a href={btnRightLink} target={rightTarget} class="button">{btnText}</a>
